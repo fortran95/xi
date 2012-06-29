7c807d5b4f6d97d38c169313d6d498aaf805bf34
lucifer <fortran95@126.com>
2012-06-29 22:38:18 +0800

    Save before sleep.

--- securelevel.py
+++ securelevel.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 # 提供一个咨询类，可以根据证书签名查明其可信程度
-# 本类和userconf/rootcerts中储存的根证书交互。
 
 BASEPATH = os.path.dirname(sys.argv[0])
 if BASEPATH != '':
@@ -10,6 +9,21 @@ ROOTCERTPATH = BASEPATH + 'userconf/rootcerts'
 
 class securelevel(object):
     def __init__(self):
-        # 缓存根证书
-        # 根证书是自签名的（以便有完整性确认）
-        
+        # 缓存证书ID-文件关系
+        # 根证书储存在   user/rootcerts 下面
+        # 其他证书储存在 user/usercerts 下面
+        pass
+    def consult(self,pubcert):
+        # 根据证书签名得知一个证书的一个或数个信任链
+        # 一个信任链是一个 dict ，结构为：
+        #  1) trust_level   最终计算出的建议信任等级，整型
+        #  2) chain         list，依次记录从证书到根证书经过的各个证书的 (ID,Subject,TrustLevel)
+        #  3) root          (ID,Subject)，信任链的根证书，如果是无效的（无根证书），则此项为空
+        pass
+
+class trustchain(object):
+    chain = []
+    def __init__(self):
+        pass
+    def append(self,pubcert):
+        # 在信任链中添加一环，返回本环的前一级
